67a46b5725ba4d249114e95e638cd9fa008559c0
Aisenyi <malisa.aisenyi@gmail.com>
2024-09-09 21:22:15 +0300

    fix: Bypass POS verification for return invoices

--- return_invoice_fix/custom_scripts/sales_invoice/sales_invoice.py
+++ return_invoice_fix/custom_scripts/sales_invoice/sales_invoice.py
@@ -1,7 +1,13 @@
 import frappe
 from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
+from frappe import _
+from frappe.utils import flt
 
 class CustomSalesInvoice(SalesInvoice):
+	def validate_pos(self):	
+		# Replace validation to always pass
+		pass
+
 	def calculate_taxes_and_totals(self):
 		from return_invoice_fix.custom_scripts.controllers.taxes_and_totals import custom_calculate_taxes_and_totals
 		custom_calculate_taxes_and_totals(self)
